7fe29c4730ffe9fc9dcb1f50db0e7829706fb700
Maks Pawlak <120831+m4ksio@users.noreply.github.com>
2021-09-14 22:14:48 -0700

    Re-organise network names

--- common.sh
+++ common.sh
@@ -5,31 +5,32 @@ set -e
 
 DATA_DIR="/data"
 
-function mainnet_data() {
+function past_data() {
 
-  MAINNET_NAME=$1
+  NETWORK_NAME=$1
   DOWNLOAD_URL=$2
   EXPECTED_SHA256=$3
 
-  INDEX_DIR="$DATA_DIR/$MAINNET_NAME"
+  NETWORK_DIR="$DATA_DIR/$NETWORK_NAME"
+  INDEX_DIR="$NETWORK_DIR/index"
 
-  echo "Checking $MAINNET_NAME data"
-  if [ ! -d "$INDEX_DIR" ]; then
+  echo "Checking $NETWORK_NAME data"
+  if [ ! -d "$NETWORK_DIR" ]; then
     TMP_FILE=$(mktemp -p "$DATA_DIR")
     wget -nv "$DOWNLOAD_URL" -O "$TMP_FILE"
     DOWNLOADED_SHA256=$(sha256sum "$TMP_FILE" | cut -d' ' -f1)
 
     if [ "$DOWNLOADED_SHA256" != "$EXPECTED_SHA256" ]; then
-      echo "$MAINNET_NAME downloaded index checksum differs"
+      echo "$NETWORK_NAME downloaded index checksum differs"
       exit 1
     fi
 
     /bin/restore-index-snapshot -c gzip -i "$INDEX_DIR" < "$TMP_FILE"
-    echo "$MAINNET_NAME data restored"
+    echo "$NETWORK_NAME data restored"
 
     rm "$TMP_FILE"
   else
-    echo "$MAINNET_NAME data already exists"
+    echo "$NETWORK_NAME data already exists"
   fi
 }
 
@@ -39,9 +40,9 @@ function live_data() {
   DOWNLOAD_BASE_URL=$2 #Make sure it doesn't end with /
   ROOT_CHECKPOINT_EXPECTED_SHA256=$3
 
-  INDEX_DIR="$DATA_DIR/live"
-  ROOT_CHECKPOINT_FILE="$INDEX_DIR/root.checkpoint"
-  PUBLIC_ROOT_INFO_DIR="$INDEX_DIR/bootstrap/public-root-information"
+  NETWORK_DIR="$DATA_DIR/$NETWORK_NAME"
+  ROOT_CHECKPOINT_FILE="$NETWORK_DIR/root.checkpoint"
+  PUBLIC_ROOT_INFO_DIR="$NETWORK_DIR/bootstrap/public-root-information"
   NODE_INFOS_FILE="$PUBLIC_ROOT_INFO_DIR/node-infos.pub.json"
   ROOT_PROTOCOL_STATE_SNAPSHOT_FILE="$PUBLIC_ROOT_INFO_DIR/root-protocol-state-snapshot.json"
 
@@ -51,7 +52,7 @@ function live_data() {
 
   echo "Checking Live $NETWORK_NAME data"
 
-  if [ ! -d "$INDEX_DIR" ]; then
+  if [ ! -d "$NETWORK_DIR" ]; then
       TMP_FILE=$(mktemp -p "$DATA_DIR")
       wget -nv "$ROOT_CHECKPOINT_DOWNLOAD_URL" -O "$TMP_FILE"
       DOWNLOADED_SHA256=$(sha256sum "$TMP_FILE" | cut -d' ' -f1)
@@ -61,7 +62,7 @@ function live_data() {
         exit 1
       fi
 
-      mkdir "$INDEX_DIR"
+      mkdir "$NETWORK_DIR"
 
       mv "$TMP_FILE" "$ROOT_CHECKPOINT_FILE"
 
