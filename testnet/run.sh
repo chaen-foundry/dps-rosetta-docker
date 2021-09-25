6835300208be98d9ddfea6b1526a684a800ee07a
Maks Pawlak <120831+m4ksio@users.noreply.github.com>
2021-09-24 20:07:50 -0700

    testnet28

--- testnet/run.sh
+++ testnet/run.sh
@@ -5,6 +5,6 @@ set -e
 
 source common.sh
 
-live_data "testnet-28"  "https://storage.googleapis.com/flow-genesis-bootstrap/devnet-28-execution" "676fe17738954c7602258696821d2a2ab24cc713f89a440c356523ebd8efca7f"
+live_data "testnet-28"  "https://storage.googleapis.com/flow-genesis-bootstrap/devnet-28-execution" "f4f74ea6d7c31e5c77bfc41c019b196769b174514c9fc16c57a1c82ac424008e"
 
 /usr/bin/supervisord -c /supervisord.conf
