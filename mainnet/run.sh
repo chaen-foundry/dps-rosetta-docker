f93eb2389200572e26afd0e009eb006e1a925da6
Maks Pawlak <120831+m4ksio@users.noreply.github.com>
2021-10-06 22:44:05 -0700

    Mainnet14 preliminary

--- mainnet/run.sh
+++ mainnet/run.sh
@@ -11,7 +11,8 @@ source common.sh
 #past_data "mainnet-6" "https://storage.googleapis.com/flow-genesis-bootstrap/dps/mainnet6-snapshot.gz" "198a38ca393e7feb7bd66a9dc23579e6c0ebd6470a76c3bfc32e13b3e64ab690"
 #past_data "mainnet-7" "https://storage.googleapis.com/flow-genesis-bootstrap/dps/mainnet7-snapshot.gz" "71df712108ca37cc03bfd7bb082195d445d13b45c3cb936778776683617b10cf"
 
-live_data "mainnet-13"  "https://storage.googleapis.com/flow-genesis-bootstrap/mainnet-13-execution" "17f925739cb914036144555499dd98e2dfac787e3ab01711304ab5e459aed825"
+#live_data "mainnet-13"  "https://storage.googleapis.com/flow-genesis-bootstrap/mainnet-13-execution" "17f925739cb914036144555499dd98e2dfac787e3ab01711304ab5e459aed825"
+live_data "mainnet-14"  "https://storage.googleapis.com/flow-genesis-bootstrap/mainnet-14-execution" "17f925739cb914036144555499dd98e2dfac787e3ab01711304ab5e459aed825"
 
 
 /usr/bin/supervisord -c /supervisord.conf
